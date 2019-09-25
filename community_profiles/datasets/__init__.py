8e9c8a20b4acac9204279af0813cab329973dac6
Nick Hand <nicholas.adam.hand@gmail.com>
2019-09-25 14:32:36 -0400

    update crime-violations to fix downloads

--- community_profiles/datasets/__init__.py
+++ community_profiles/datasets/__init__.py
@@ -2,6 +2,7 @@ EPSG = 2272
 
 from .regions import *
 from .rebuild import *
-from .Facility import * 
-from .septa import * 
-from .crime_violations import * 
+from .Facility import *
+from .septa import *
+from .crime_violations import *
+
--- community_profiles/datasets/crime_violations.py
+++ community_profiles/datasets/crime_violations.py
@@ -1,14 +1,20 @@
 import carto2gpd
 import geopandas as gpd
+import pandas as pd
 from . import EPSG
-from .core import Dataset
+from .core import Dataset, geocode, replace_missing_geometries
+from .regions import *
+
+__all__ = [
+    "CrimeIncidents",
+    "Shootings",
+    "ParkingViolations",
+    "StreetCodeViolations",
+    "LIViolations",
+]
 
 
-
-__all__ = ["Crime", "Shootings", "ParkingV", "StreetCodeV", "L_IV"]
-
-
-class Crime(Dataset):
+class CrimeIncidents(Dataset):
     """
     Crime Incidents
     Available: 2006-Present, Updated Daily 
@@ -16,18 +22,42 @@ class Crime(Dataset):
     
     Source
     ------
-    https://phl.carto.com/api/v2/sql?q=SELECT+*+FROM+incidents_part1_part2&filename=incidents_part1_part2&format=shp&skipfields=cartodb_id
+    https://www.opendataphilly.org/dataset/crime-incidents
     """
+
+    date_columns = ["dispatch_date_time"]
 
     @classmethod
     def download(cls, **kwargs):
-        url = "https://phl.carto.com/api/v2/sql"
-        where = "year(dispatch_date) = 2018"  ### doesn't work. function year(text) does not exist... How to change to datetime??
-        gdf = carto2gpd.get(url, "incidents_part1_part2",  where=where)
 
-        return gdf.to_crs(epsg=EPSG) 
-        
-    
+        # the raw data
+        fields = [
+            "dc_dist",
+            "dc_key",
+            "dispatch_date_time",
+            "location_block",
+            "psa",
+            "text_general_code",
+            "ucr_general",
+        ]
+        url = "https://phl.carto.com/api/v2/sql"
+        where = "extract(year from dispatch_date_time) = 2018"
+        gdf = carto2gpd.get(url, "incidents_part1_part2", fields=fields, where=where)
+
+        return (
+            replace_missing_geometries(gdf)
+            .to_crs(epsg=EPSG)
+            .pipe(geocode, ZIPCodes.get())
+            .pipe(geocode, Neighborhoods.get())
+            .pipe(geocode, PUMAs.get())
+            .assign(
+                dispatch_date_time=lambda df: pd.to_datetime(df.dispatch_date_time),
+                year=lambda df: df.dispatch_date_time.dt.year,
+            )
+            .sort_values("dispatch_date_time", ascending=False)
+            .reset_index(drop=True)
+        )
+
 
 class Shootings(Dataset):
     """
@@ -37,20 +67,38 @@ class Shootings(Dataset):
 
     Source
     ------
-    https://phl.carto.com/api/v2/sql?q=SELECT+*+FROM+shootings&filename=shootings&format=shp&skipfields=cartodb_id
+    https://www.opendataphilly.org/dataset/shooting-victims
     """
+
+    date_columns = ["date"]
 
     @classmethod
     def download(cls, **kwargs):
-        
+
         url = "https://phl.carto.com/api/v2/sql"
         where = "year = 2018"
-        gdf = carto2gpd.get(url, "shootings",  where=where)
+        gdf = carto2gpd.get(url, "shootings", where=where)
 
-        return gdf.to_crs(epsg=EPSG) 
-        
-    
-class ParkingV(Dataset):
+        return (
+            replace_missing_geometries(gdf)
+            .fillna(np.nan)
+            .to_crs(epsg=EPSG)
+            .pipe(geocode, ZIPCodes.get())
+            .pipe(geocode, Neighborhoods.get())
+            .pipe(geocode, PUMAs.get())
+            .assign(
+                time=lambda df: df.time.replace("<Null>", np.nan).fillna("00:00:00"),
+                date=lambda df: pd.to_datetime(
+                    df.date_.str.slice(0, 10).str.cat(df.time, sep=" ")
+                ),
+            )
+            .drop(labels=["point_x", "point_y", "date_", "time", "objectid"], axis=1)
+            .sort_values("date", ascending=False)
+            .reset_index(drop=True)
+        )
+
+
+class ParkingViolations(Dataset):
     """
     Parking Violations
     Available: 2012-2017
@@ -58,20 +106,33 @@ class ParkingV(Dataset):
 
     Source
     ------
-    https://phl.carto.com/api/v2/sql?q=SELECT+*+FROM+parking_violations&filename=parking_violations&format=shp&skipfields=cartodb_id
+    https://www.opendataphilly.org/dataset/parking-violations
     """
+
+    date_columns = ["issue_datetime"]
 
     @classmethod
     def download(cls, **kwargs):
         url = "https://phl.carto.com/api/v2/sql"
-        where = "year(issue_datetime) = 2017" ### function year(timestamp without time zone) does not exist
-        gdf = carto2gpd.get(url, "parking_violations",  where=where)
+        where = "extract(year from issue_datetime) = 2017"
+        gdf = carto2gpd.get(url, "parking_violations", where=where)
 
-        return gdf.to_crs(epsg=EPSG) 
-        
-            
-        
-class StreetCodeV(Dataset):
+        return (
+            replace_missing_geometries(gdf)
+            .to_crs(epsg=EPSG)
+            .pipe(geocode, ZIPCodes.get())
+            .pipe(geocode, Neighborhoods.get())
+            .pipe(geocode, PUMAs.get())
+            .assign(
+                issue_datetime=lambda df: pd.to_datetime(df.issue_datetime),
+                year=lambda df: df.issue_datetime.dt.year,
+            )
+            .sort_values("issue_datetime", ascending=False)
+            .reset_index(drop=True)
+        )
+
+
+class StreetCodeViolations(Dataset):
     """
     Code Violation Notices issued from the Street's department
     Available: 07-21-2009 - 12-27-2048???
@@ -79,21 +140,33 @@ class StreetCodeV(Dataset):
 
     Source
     ------
-    https://phl.carto.com/api/v2/sql?q=SELECT+*+FROM+streets_code_violation_notices&
-    filename=streets_code_violation_notices&format=shp&skipfields=cartodb_id
+    https://www.opendataphilly.org/dataset/code-violation-notices
     """
+
+    date_columns = ["date_added"]
 
     @classmethod
     def download(cls, **kwargs):
         url = "https://phl.carto.com/api/v2/sql"
-        where = "year(date_added) = 2017" ### function year(date) does not exist
-        gdf = carto2gpd.get(url, "streets_code_violation_notices",  where=where)
+        where = "extract(year from date_added) = 2017"
+        gdf = carto2gpd.get(url, "streets_code_violation_notices", where=where)
 
-        return gdf.to_crs(epsg=EPSG) 
-        
-    
-    
-class L_IV(Dataset):
+        return (
+            replace_missing_geometries(gdf)
+            .to_crs(epsg=EPSG)
+            .pipe(geocode, ZIPCodes.get())
+            .pipe(geocode, Neighborhoods.get())
+            .pipe(geocode, PUMAs.get())
+            .assign(
+                date_added=lambda df: pd.to_datetime(df.date_added),
+                year=lambda df: df.date_added.dt.year,
+            )
+            .sort_values("date_added", ascending=False)
+            .reset_index(drop=True)
+        )
+
+
+class LIViolations(Dataset):
     """
     Violations issued by the Department of Licenses and Inspections 
     in reference to the Philadelphia Building Construction and OccupancyCode
@@ -102,17 +175,28 @@ class L_IV(Dataset):
     
     Source
     ------
-    https://phl.carto.com/api/v2/sql?q=SELECT+*+FROM+li_violations&filename=li_violations&format=shp&skipfields=cartodb_id
+    https://www.opendataphilly.org/dataset/licenses-and-inspections-violations
     """
+
+    date_columns = ["violationdate"]
 
     @classmethod
     def download(cls, **kwargs):
         url = "https://phl.carto.com/api/v2/sql"
-        where = "year(violationdate) = 2017" ### function year(date) does not exist
-        gdf = carto2gpd.get(url, "li_violations",  where=where)
+        where = "extract(year from violationdate) = 2017"
+        gdf = carto2gpd.get(url, "li_violations", where=where)
 
-        return gdf.to_crs(epsg=EPSG) 
-               
-            
-        
-    
+        return (
+            replace_missing_geometries(gdf)
+            .to_crs(epsg=EPSG)
+            .pipe(geocode, ZIPCodes.get())
+            .pipe(geocode, Neighborhoods.get())
+            .pipe(geocode, PUMAs.get())
+            .assign(
+                violationdate=lambda df: pd.to_datetime(df.violationdate),
+                year=lambda df: df.violationdate.dt.year,
+            )
+            .sort_values("violationdate", ascending=False)
+            .reset_index(drop=True)
+        )
+
