235a12a81ba9abcc2167ff79809319cf90f638ce
Nick Hand <nicholas.adam.hand@gmail.com>
2019-11-08 13:03:31 -0500

    remove per capita income

--- community_profiles/datasets/census/__init__.py
+++ community_profiles/datasets/census/__init__.py
@@ -13,7 +13,6 @@ from .medianage import *
 from .medianearnings import *
 from .medianhouseholdincome import *
 from .mobility import *
-from .percapitaincome import *
 from .population import *
 from .poverty import *
 from .race import *
